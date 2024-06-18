74b066b505a969cd04062e54993f5f2a248b26b3
Andrei Cristinaru <andreicristinaru@yahoo.com>
2024-06-18 19:54:27 +0300

    Create project

---
+++ src/main/java/com/example/shiftmanajer/ShiftManajerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftmanajer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftManajerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftManajerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shiftmanajer/ShiftManajerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiftmanajer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftManajerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
